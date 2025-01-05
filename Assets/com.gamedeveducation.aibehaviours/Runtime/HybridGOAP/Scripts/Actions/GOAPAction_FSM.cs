6317ed949c34e793d28e76e1c158e8db88015dc4
Haonan Xu <xhn20001106@outlook.com>
2025-01-06 01:07:37 +0800

    Implemented abstract class GOAPAction_FSM
    - Implemented abstract class GOAPAction_FSM: internal class InternalState_StateMachineFinished, linked FSM, OnInitialize (bind FSM blackboard, custom configure FSM, two internal states to hold custom callback functions when FSM is finished and failed), two callback functions: On FSM finished or failed (both with optional override), On Start(reset FSM)/Continue(do nothing)/Stop(reset FSM)/Tick(tick FSM)

--- Assets/com.gamedeveducation.aibehaviours/Runtime/HybridGOAP/Scripts/Actions/GOAPAction_FSM.cs
+++ Assets/com.gamedeveducation.aibehaviours/Runtime/HybridGOAP/Scripts/Actions/GOAPAction_FSM.cs
@@ -1,21 +1,102 @@
+using CommonCore;
+using StateMachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace HybridGOAP
 {
-    public class GOAPAction_FSM : MonoBehaviour
+    public abstract class GOAPAction_FSM : GOAPActionBase
     {
-        // Start is called before the first frame update
-        void Start()
+        class InternalState_StateMachineFinished : SMStateBase
         {
-        
+            System.Action CallbackFn;
+
+            internal InternalState_StateMachineFinished(System.Action InCallbackFn) :
+                base(null) 
+            {
+                CallbackFn = InCallbackFn;
+            }
+
+            protected override ESMStateStatus OnEnterInternal(Blackboard<FastName> InBlackboard)
+            {
+                CallbackFn();
+
+                return ESMStateStatus.Finished;
+            }
+
+            protected override void OnExitInternal(Blackboard<FastName> InBlackboard)
+            {
+            }
+
+            protected override ESMStateStatus OnTickInternal(Blackboard<FastName> InBlackboard, float InDeltaTime)
+            {
+                return ESMStateStatus.Finished;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        protected SMInstance LinkedStateMachine = new();
+
+        protected ISMState InternalState_Failed { get; private set; }
+        protected ISMState InternalState_Finished { get; private set; }
+
+        protected override void OnInitialize()
         {
-        
+            InternalState_Finished = new InternalState_StateMachineFinished(InternalOnStateMachineCompleted_Finished);
+            InternalState_Failed = new InternalState_StateMachineFinished(InternalOnStateMachineCompleted_Failed);
+
+            LinkedStateMachine.BindToBlackboard(LinkedBlackboard);
+
+            ConfigureFSM();
         }
+
+        void InternalOnStateMachineCompleted_Failed()
+        {
+            // reset FSM if failed
+            LinkedStateMachine.Reset();
+            OnStateMachineReset();
+
+            OnStateMachineCompleted_Failed();
+        }
+
+        void InternalOnStateMachineCompleted_Finished()
+        {
+            OnStateMachineCompleted_Finished();
+        }
+
+        protected override void OnStartAction()
+        {
+            LinkedStateMachine.Reset();
+            OnStateMachineReset();
+        }
+
+        protected override void OnContinueAction()
+        {
+        }
+
+        protected override void OnStopAction()
+        {
+            LinkedStateMachine.Reset();
+            OnStateMachineReset();
+        }
+
+        protected override void OnTickAction(float InDeltaTime)
+        {
+            LinkedStateMachine.Tick(InDeltaTime);
+        }
+
+        protected override void GatherDebugDataInternal(IGameDebugger InDebugger, bool bInIsSelected)
+        {
+            if (!bInIsSelected)
+                return;
+
+            LinkedStateMachine.GatherDebugData(InDebugger, bInIsSelected);
+        }
+
+        protected abstract void ConfigureFSM();
+
+        protected virtual void OnStateMachineCompleted_Failed() { }
+        protected virtual void OnStateMachineCompleted_Finished() { }
+        protected virtual void OnStateMachineReset() { }
     }
 }
